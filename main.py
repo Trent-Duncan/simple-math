36af3ec65f6a5fe9514e19aa8896fc454e31281e
Trent-Duncan <duncantr131@northwested.org>
2024-09-25 13:35:39 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# Trent Duncan
+# 9/25/24
+# basic math practice1
+
+num1=float(5) 
+num2= float(2)
+sum= num1 + num2
+diffrence= num1 - num2
+product= num1 * num2
+quotient= num1 / num2
+
+print(f'{num1} plus {num2} = {sum}')
+print(f'{num1} minus {num2} = {diffrence}')
+print(f' {num1} * {num2} = {product}')
+print(f'{num1} devided by {num2} = {quotient}')
